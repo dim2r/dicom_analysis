768011687b0f8e519c72ba6ab73c7eec1ccee094
dim2r <dim2r@yandex.ru>
2020-05-01 11:36:19 +0400

    polygons support

--- augment_images.py
+++ augment_images.py
@@ -3,31 +3,35 @@ import os
 import sys
 import pathlib
 
-path=u"C:\\_Denis\\train_data\\MarkSet2_original"
-outdir_ = path+"_augment"
+in_path= u"C:\\_Denis\\train_data\\MarkSet2_original"
+out_path = in_path.replace('_original', '_augment')
 
-for curdir, subdirs, files in os.walk(path):
+for curdir, subdirs, files in os.walk(in_path):
 
     for n, image_name in enumerate(files):
         _, ext = os.path.splitext(image_name)
 
-        outdir = curdir.replace(path,outdir_)
+        outdir = curdir.replace(in_path, out_path)
         if ext=='.jpg' or  ext=='.gif' or  ext=='.png':
-            pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
-            print(f'{outdir} {image_name}')
-            colorImage = Image.open(curdir+'\\'+image_name)
-            rotated = colorImage.rotate(3)
-            rotated.save(outdir +'\\r3.'+image_name )
-            rotated = colorImage.rotate(-3)
-            rotated.save(outdir +'\\r-3.'+image_name )
+            ff1=outdir +'\\r3.'+image_name
+            ff2=outdir +'\\r-3.'+image_name
 
-            mode = colorImage.mode
-            if len(mode) == 1:  # L, 1
-                new_background = (255)
-            if len(mode) == 3:  # RGB
-                new_background = (255, 255, 255)
-            if len(mode) == 4:  # RGBA, CMYK
-                new_background = (255, 255, 255, 255)
+            if not os.path.isfile(ff1) or not  os.path.isfile(ff2) :
+                pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+                print(f'{outdir} {image_name}')
+                colorImage = Image.open(curdir+'\\'+image_name)
+                rotated = colorImage.rotate(3)
+                rotated.save( ff1)
+                rotated = colorImage.rotate(-3)
+                rotated.save( ff2 )
+
+            # mode = colorImage.mode
+            # if len(mode) == 1:  # L, 1
+            #     new_background = (255)
+            # if len(mode) == 3:  # RGB
+            #     new_background = (255, 255, 255)
+            # if len(mode) == 4:  # RGBA, CMYK
+            #     new_background = (255, 255, 255, 255)
 
             # img1 = colorImage.resize((512, 462), Image.ANTIALIAS)
             # newImage = Image.new(mode, (512, 512), new_background)
